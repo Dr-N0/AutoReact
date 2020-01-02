fb831352ce0c8811a89ef9bb6ee908f3b2a53c41
Dr-N0 <beckettjenen@gmail.com>
2020-01-02 14:48:30 -0500

    Knocked out the chat feature in a day, bot is ready to send to OpenShift. Might run though lint catcher again though

--- main.js
+++ main.js
@@ -124,6 +124,64 @@ function setUserVariables(){
         console.log(err.message);
     }
 }
+// username, reaction
+function chatCommunication(text, username, a, b, option){
+	if (option == "add") {
+		attachments_json = [
+	        {
+	            "text": b,
+	            "fallback": "You are unable to post this to Jumpstart",
+	            "callback_id": "send_to_auto_react",
+	            "color": "#800080",
+	            "attachment_type": "default",
+	            "actions": [
+	                {
+	                    "name": "yes_a_ar",
+	                    "text": "Yes",
+	                    "style": "primary",
+	                    "type": "button",
+	                    "value": a+"~"+b
+	                },
+	                {
+	                    "name": "no_a_ar",
+	                    "text": "No",
+	                    "style": "danger",
+	                    "type": "button",
+	                    "value": "no"
+	                }
+	            ]
+	        }
+	    ]
+    	web.chat.postMessage({channel: username, text: "<@" + username + "> wants to add :" + b + ": to your name.\nDisclaimer: This emote will be reacted on every message you post.\nDo you want this emote to be added?", attachments: attachments_json}).catch(console.error);
+	}else if(option == "delete"){
+		attachments_json = [
+	        {
+	            "text": b,
+	            "fallback": "You are unable to delete this emote",
+	            "callback_id": "send_to_auto_react",
+	            "color": "#800080",
+	            "attachment_type": "default",
+	            "actions": [
+	                {
+	                    "name": "yes_d_ar",
+	                    "text": "Yes",
+	                    "style": "primary",
+	                    "type": "button",
+	                    "value": a+"~"+b
+	                },
+	                {
+	                    "name": "no_d_ar",
+	                    "text": "No",
+	                    "style": "danger",
+	                    "type": "button",
+	                    "value": "no"
+	                }
+	            ]
+	        }
+	    ]
+    	web.chat.postMessage({channel: username, text: "<@" + username + "> wants to delete :" + b + ": from your name.\nDisclaimer: This emote will no longer be reacted on every message you post.\nDo you want this emote to be deleted?", attachments: attachments_json}).catch(console.error);
+	}
+}
 
 // ROUTES
 app.post("/add", (req, res) => {
@@ -135,14 +193,13 @@ app.post("/add", (req, res) => {
         && comTokens[1].lastIndexOf(":") != 0) {
         var u = comTokens[0].split(/[@|]/);
         var r = comTokens[1].split(/[::]/);
-        console.log(u[1])
-        console.log(r[1])
-        createReact(u[1], r[1]);
-        setUserVariables();
+        console.log(u[1]);
+        console.log(r[1]);
+        chatCommunication(text, info, u[1], r[1], "add");
     }else{
         res.send("Syntax Error: The command you entered was incorrect");
     }
-    res.send("Addition sent ( "+ text + " )");
+    res.send("( "+ text + " )");
 });
 
 app.post("/delete", (req, res) => {
@@ -157,15 +214,35 @@ app.post("/delete", (req, res) => {
         if (info == u[1]) {
             console.log(u[1])
             console.log(r[1])
-            deleteReact(u[1], r[1]);
-            setUserVariables();
+            chatCommunication(text, info, u[1], r[1], "delete");
         }else{
             res.send("You are not authorized to send that command!");
         }
     }else{
         res.send("Syntax Error: The command you entered was incorrect");
     }
-    res.send("");
+    res.send("( "+ text + " )");
+});
+
+app.post("/interactivity", (req, res) => {
+	var reqParse = JSON.parse(req.body.payload).actions[0];
+	var reqParseValue = reqParse.value.split("~");
+	var reqName = reqParse.name;
+	var reqU = reqParseValue[0];
+	var reqR = reqParseValue[1];
+    console.log(reqParse);
+    console.log(reqU);
+    console.log(reqR);
+    if (reqName == "yes_a_ar") {
+    	createReact(reqU, reqR);
+        setUserVariables();
+    }else if(reqName == "yes_d_ar"){
+    	deleteReact(reqU, reqR);
+        setUserVariables();
+    }else{
+    	res.send("No U");
+    }
+    res.send("Route Finished");
 });
 
 app.listen(3000, () => {
@@ -174,9 +251,6 @@ app.listen(3000, () => {
 
 // SLACK EVENTS API
 slackEvents.on("message", (event) => {
-    // if (event.subtype != "bot_message") {
-    //     web.chat.postMessage({channel: event.channel, text: event.user}).catch(console.error);
-    // }
     console.log(event);
     console.log(re);
     console.log(ulist);
