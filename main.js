dd6b393454b0d589a20b4b029cd04aebf6fc099e
Dr-N0 <beckettjenen@gmail.com>
2020-10-22 19:53:41 -0400

    Funny random go brr

--- main.js
+++ main.js
@@ -322,11 +322,14 @@ slackEvents.on("message", (event) => {
     // Lowest complexity since the db needs to be checked though at least once.
     for (var j = 0; j < re.length; j++) {
         if (event.user == ulist[j]) {
-            web.reactions.add({
-                channel: event.channel,
-                name: re[j].reac,
-                timestamp: event.ts
-            }).catch(console.error);
+            const rollTheDice = Math.random() > 0.7;
+            if (rollTheDice) {
+                web.reactions.add({
+                    channel: event.channel,
+                    name: re[j].reac,
+                    timestamp: event.ts
+                }).catch(console.error);
+            }
         }
     }
 
